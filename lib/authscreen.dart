9d6d67155c3f2e1104a0123c0a24cc6d54af82f0
Waheguruji <praneetsingh434@yahoo.com>
2024-02-05 22:08:24 +0530

    Added Responsive Sizer to Login Page

--- lib/authscreen.dart
+++ lib/authscreen.dart
@@ -38,222 +38,222 @@ class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: 
-        SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                width: 390,
-                height: 844,
-                clipBehavior: Clip.antiAlias,
-                decoration: const BoxDecoration(color: Colors.white),
-                child: Stack(
-                  children: [
-                    Positioned(
-                      left: 96,
-                      top: 49,
-                      child: SizedBox(
-                        width: 198,
-                        height: 17,
-                        child: Stack(
-                          children: [
-                            Positioned(
-                              left: 0,
-                              top: 0,
-                              child: Container(
-                                width: 198,
-                                height: 17,
-                                decoration: ShapeDecoration(
-                                  color: const Color(0xFFD9D9D9),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20),
-                                  ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: 390,
+              height: 844,
+              clipBehavior: Clip.antiAlias,
+              decoration: const BoxDecoration(color: Colors.white),
+              child: Stack(
+                children: [
+                  Positioned(
+                    left: 96,
+                    top: 49,
+                    child: SizedBox(
+                      width: 198,
+                      height: 17,
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            left: 0,
+                            top: 0,
+                            child: Container(
+                              width: 198,
+                              height: 17,
+                              decoration: ShapeDecoration(
+                                color: const Color(0xFFD9D9D9),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
                                 ),
                               ),
                             ),
-                            Positioned(
-                              left: 0,
-                              top: 0,
-                              child: Container(
-                                width: 98,
-                                height: 17,
-                                decoration: ShapeDecoration(
-                                  color: const Color(0xFF259ACD),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20),
-                                  ),
+                          ),
+                          Positioned(
+                            left: 0,
+                            top: 0,
+                            child: Container(
+                              width: 98,
+                              height: 17,
+                              decoration: ShapeDecoration(
+                                color: const Color(0xFF259ACD),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
                                 ),
                               ),
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
-                    const Positioned(
-                      left: 123,
-                      top: 31,
-                      child: Text.rich(
-                        TextSpan(
-                          children: [
-                            TextSpan(
-                              text: 'Verification Process ',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 12,
-                                fontFamily: 'Inter',
-                                fontWeight: FontWeight.w500,
-                                height: 0,
-                              ),
+                  ),
+                  const Positioned(
+                    left: 123,
+                    top: 31,
+                    child: Text.rich(
+                      TextSpan(
+                        children: [
+                          TextSpan(
+                            text: 'Verification Process ',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12,
+                              fontFamily: 'Inter',
+                              fontWeight: FontWeight.w500,
+                              height: 0,
                             ),
-                            TextSpan(
-                              text: '50%',
-                              style: TextStyle(
+                          ),
+                          TextSpan(
+                            text: '50%',
+                            style: TextStyle(
+                              color: Color(0xFF259ACD),
+                              fontSize: 12,
+                              fontFamily: 'Inter',
+                              fontWeight: FontWeight.w500,
+                              height: 0,
+                            ),
+                          ),
+                        ],
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Positioned(
+                    left: 94,
+                    top: 148,
+                    child: SizedBox(
+                      width: 200,
+                      height: 200,
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            left: 0,
+                            top: 0,
+                            child: Container(
+                              width: 200,
+                              height: 200,
+                              decoration: const ShapeDecoration(
                                 color: Color(0xFF259ACD),
-                                fontSize: 12,
-                                fontFamily: 'Inter',
-                                fontWeight: FontWeight.w500,
-                                height: 0,
+                                shape: OvalBorder(),
                               ),
-                            ),
-                          ],
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    Positioned(
-                      left: 94,
-                      top: 148,
-                      child: SizedBox(
-                        width: 200,
-                        height: 200,
-                        child: Stack(
-                          children: [
-                            Positioned(
-                              left: 0,
-                              top: 0,
-                              child: Container(
-                                width: 200,
-                                height: 200,
-                                decoration: const ShapeDecoration(
-                                  color: Color(0xFF259ACD),
-                                  shape: OvalBorder(),
-                                ),
-                                child: ClipOval(
-                                  child: Image.asset(
-                                    'assets/face.png',
-                                    width: 200,
-                                    height: 200,
-                                    fit: BoxFit.cover,
-                                  ),
+                              child: ClipOval(
+                                child: Image.asset(
+                                  'assets/face.png',
+                                  width: 200,
+                                  height: 200,
+                                  fit: BoxFit.cover,
                                 ),
                               ),
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
-                    const Positioned(
-                      left: 70,
-                      top: 393,
+                  ),
+                  const Positioned(
+                    left: 70,
+                    top: 393,
+                    child: Text(
+                      'Biometric Authentication',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 24,
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w500,
+                        height: 0,
+                      ),
+                    ),
+                  ),
+                  const Positioned(
+                    left: 77,
+                    top: 425,
+                    child: SizedBox(
+                      width: 234,
                       child: Text(
-                        'Biometric Authentication',
+                        'Your face will be scanned to verify your identity when signing in',
+                        textAlign: TextAlign.center,
                         style: TextStyle(
                           color: Colors.black,
-                          fontSize: 24,
+                          fontSize: 14,
                           fontFamily: 'Inter',
-                          fontWeight: FontWeight.w500,
+                          fontWeight: FontWeight.w400,
                           height: 0,
                         ),
                       ),
                     ),
-                    const Positioned(
-                      left: 77,
-                      top: 425,
-                      child: SizedBox(
-                        width: 234,
-                        child: Text(
-                          'Your face will be scanned to verify your identity when signing in',
+                  ),
+                  Positioned(
+                    left: 74,
+                    top: 732,
+                    child: ElevatedButton(
+                      onPressed: _authenticate,
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor:
+                            const Color(0xFF259ACD), // Button color
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                      ),
+                      child: Container(
+                        width: 242,
+                        height: 53,
+                        alignment: Alignment.center,
+                        child: const Text(
+                          'Verify My Fingerprint',
                           textAlign: TextAlign.center,
                           style: TextStyle(
-                            color: Colors.black,
+                            color: Colors.white,
                             fontSize: 14,
                             fontFamily: 'Inter',
-                            fontWeight: FontWeight.w400,
-                            height: 0,
+                            fontWeight: FontWeight.w500,
                           ),
                         ),
                       ),
                     ),
-                    Positioned(
-                      left: 74,
-                      top: 732,
-                      child: ElevatedButton(
-                        onPressed: _authenticate,
-                        style: ElevatedButton.styleFrom(
-                          foregroundColor: const Color(0xFF259ACD), // Button color
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(40),
-                          ),
-                        ),
-                        child: Container(
-                          width: 242,
-                          height: 53,
-                          alignment: Alignment.center,
-                          child: const Text(
-                            'Verify My Fingerprint',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 14,
-                              fontFamily: 'Inter',
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                        ),
+                  ),
+                  Positioned(
+                    left: 332,
+                    top: 42,
+                    child: Container(
+                      width: 30,
+                      height: 30,
+                      decoration: const ShapeDecoration(
+                        color: Color(0xFF259ACD),
+                        shape: OvalBorder(),
                       ),
                     ),
-                    Positioned(
-                      left: 332,
-                      top: 42,
+                  ),
+                  Positioned(
+                    left: 333,
+                    top: 57.14,
+                    child: Transform(
+                      transform: Matrix4.identity()
+                        ..translate(0.0, 0.0)
+                        ..rotateZ(-0.79),
                       child: Container(
-                        width: 30,
-                        height: 30,
-                        decoration: const ShapeDecoration(
-                          color: Color(0xFF259ACD),
-                          shape: OvalBorder(),
+                        width: 20,
+                        height: 20,
+                        padding: const EdgeInsets.all(2.50),
+                        clipBehavior: Clip.antiAlias,
+                        decoration: const BoxDecoration(),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Image.asset('assets/Vector.png'),
+                          ],
                         ),
                       ),
                     ),
-                    Positioned(
-                      left: 333,
-                      top: 57.14,
-                      child: Transform(
-                        transform: Matrix4.identity()
-                          ..translate(0.0, 0.0)
-                          ..rotateZ(-0.79),
-                        child: Container(
-                          width: 20,
-                          height: 20,
-                          padding: const EdgeInsets.all(2.50),
-                          clipBehavior: Clip.antiAlias,
-                          decoration: const BoxDecoration(),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Image.asset('assets/Vector.png'),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
+      ),
     );
   }
 }
