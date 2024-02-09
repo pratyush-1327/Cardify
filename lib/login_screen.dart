51b20989cd8efba9c23022fd088c4ff8f5ff1af3
pratyush-1327 <pratyushsingh2005@gmail.com>
2024-02-09 11:43:36 +0530

    Fixed text fields,bugs; added google logo

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -12,9 +12,7 @@ class LoginScreen extends StatelessWidget {
           child: Container(
               decoration: const BoxDecoration(
                 image: DecorationImage(
-                    image: NetworkImage(
-                        "https://raw.githubusercontent.com/pratyush-1327/counter_new/fdaf4220883be86d74d4109739a2a72e0c119052/login%20page.png"),
-                    fit: BoxFit.fill),
+                    image: AssetImage("assets/bg.png"), fit: BoxFit.fill),
               ),
               child: Stack(children: <Widget>[
                 Align(
@@ -23,7 +21,7 @@ class LoginScreen extends StatelessWidget {
                       'Welcome to',
                       //textAlign: TextAlign.left,
                       style: TextStyle(
-                          shadows: [
+                          shadows: const [
                             Shadow(
                               color: Color.fromARGB(48, 0, 0,
                                   0), // Choose the color of the shadow
@@ -33,7 +31,7 @@ class LoginScreen extends StatelessWidget {
                                   2.0), // Set the horizontal and vertical offset for the shadow
                             ),
                           ],
-                          color: Color.fromRGBO(0, 0, 0, 1),
+                          color: const Color.fromRGBO(0, 0, 0, 1),
                           fontFamily: 'Prompt',
                           fontSize: 23.sp,
                           fontWeight: FontWeight.w500,
@@ -89,41 +87,60 @@ class LoginScreen extends StatelessWidget {
                                   style: TextStyle(
                                       fontSize: 18.sp,
                                       color:
-                                          const Color.fromARGB(255, 0, 0, 0)))),
+                                          const Color.fromARGB(255, 255, 255, 255)))),
                         ]))),
                 Container(
                     alignment: Alignment(0.5.sp, 3.6.sp),
                     child: SizedBox(
-                        height: 30.sp,
+                        height: 22.sp,
+                        width: 60.sp,
                         child: Stack(children: <Widget>[
-                          TextButton(
+                          ElevatedButton(
                               style: ButtonStyle(
+                                  backgroundColor: const MaterialStatePropertyAll(
+                                      Color.fromARGB(255, 255, 255, 255)),
                                   padding: MaterialStateProperty.all<EdgeInsets>(
                                       const EdgeInsets.all(10)),
-                                  shape:
-                                      MaterialStateProperty.all<RoundedRectangleBorder>(
-                                          RoundedRectangleBorder(
-                                              borderRadius:
-                                                  BorderRadius.circular(20.0),
-                                              side: const BorderSide(
-                                                  color: Color.fromARGB(
-                                                      255, 0, 0, 0))))),
+                                  shape: MaterialStateProperty.all<
+                                          RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(20.0),
+                                          side: const BorderSide(
+                                              color:
+                                                  Color.fromARGB(255, 0, 0, 0))))),
                               onPressed: () {},
-                              child: const Text(
-                                  "         Login with Google         ",
-                                  style: TextStyle(
-                                      fontSize: 16,
-                                      color: Color.fromARGB(255, 0, 0, 0)))),
+                              child: Row(children: [
+                                Image.asset("assets/google.png"),
+                                const Text("    Login with Google",
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        color: Color.fromARGB(255, 0, 0, 0)))
+                              ])),
                         ]))),
                 Align(
-                    alignment: Alignment(0.5.sp, 3.9.sp),
+                    alignment: Alignment(-2.55.sp, 3.9.sp),
                     child: Text(
-                      'Don’t have account?  Register Now',
+                      'Don’t have account?',
                       textAlign: TextAlign.left,
                       style: TextStyle(
                           color: Colors.black,
                           fontFamily: 'Inter',
-                          fontSize: 16.5.sp,
+                          fontSize: 15.sp,
+                          letterSpacing:
+                              0 /*percentages not used in flutter. defaulting to zero*/,
+                          fontWeight: FontWeight.normal,
+                          height: 5.sp),
+                    )),
+                Align(
+                    alignment: Alignment(2.5.sp, 3.9.sp),
+                    child: Text(
+                      'Register Now',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                          color: const Color.fromARGB(255, 12, 84, 252),
+                          fontFamily: 'Inter',
+                          fontSize: 15.sp,
                           letterSpacing:
                               0 /*percentages not used in flutter. defaulting to zero*/,
                           fontWeight: FontWeight.normal,
@@ -133,40 +150,32 @@ class LoginScreen extends StatelessWidget {
                     alignment: Alignment(0.5.sp, 0.8.sp),
                     child: SizedBox(
                         width: 235,
-                        height: 35,
                         child: Stack(children: <Widget>[
                           TextFormField(
+                            style: const TextStyle(color: Colors.black),
                             decoration: const InputDecoration(
-                              border: UnderlineInputBorder(),
-                              labelText: 'Email',
-                              //focusColor: Color.fromARGB(255, 0, 0, 0),
-                            ),
+                                border: UnderlineInputBorder(),
+                                labelText: 'Email',
+                                labelStyle: TextStyle(
+                                    color: Colors.black, fontFamily: 'Inter')
+                                //focusColor: Color.fromARGB(255, 0, 0, 0),
+                                ),
                           ),
-                          Positioned(
-                              top: 23.sp,
-                              left: 0.sp,
-                              child: Divider(
-                                  color: Color.fromRGBO(0, 0, 0, 1),
-                                  thickness: 1)),
                         ]))),
                 Align(
                     alignment: Alignment(0.5.sp, 1.8.sp),
                     child: SizedBox(
                         width: 235,
-                        height: 37,
                         child: Stack(children: <Widget>[
                           TextFormField(
+                            style: const TextStyle(
+                                color: Colors.black, fontFamily: 'Inter'),
                             decoration: const InputDecoration(
-                              border: UnderlineInputBorder(),
-                              labelText: 'Password',
-                            ),
+                                focusColor: Colors.black,
+                                border: UnderlineInputBorder(),
+                                labelText: 'Password',
+                                labelStyle: TextStyle(color: Colors.black)),
                           ),
-                          const Positioned(
-                              top: 27,
-                              left: 0,
-                              child: Divider(
-                                  color: Color.fromRGBO(0, 0, 0, 1),
-                                  thickness: 1)),
                         ]))),
               ])));
     });
